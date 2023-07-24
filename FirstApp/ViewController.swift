2bb9bdf25a0d38b6d93b9ed455c11dd6111516ed
ilka <ilka.ilkoy@gmail.com>
2023-07-24 09:20:22 +0200

    Logic changes

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Light {
+    case red
+    case yellow
+    case green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    private var light: Light?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 70
+        yellowLight.layer.cornerRadius = 70
+        greenLight.layer.cornerRadius = 70
+        
     }
-
-
+    
+    @IBAction func startButtonDidTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+        switch light{
+        case .red:
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            light = .yellow
+        case .yellow:
+            yellowLight.alpha = 1
+            redLight.alpha = 0.3
+            light = .green
+        case .green:
+            greenLight.alpha = 1
+            yellowLight.alpha = 0.3
+            light = .red
+        default:
+            light = .red
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+            light = .yellow
+        }
+    }
+    
+    
 }
 
